cabee24e7749344e2291f4ecc30b483c2a53a219
yassine mhedhbi <yassine.kaitokid.mhedhbi@gmail.com>
2020-03-25 03:51:06 -0400

    feat: add basic input and skybox

---
+++ Assets/scripts/rocketShip.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocketShip : MonoBehaviour
+{
+    Rigidbody body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("foce up");
+            body.AddRelativeForce(Vector3.up * 10);
+        }
+    }
+}
